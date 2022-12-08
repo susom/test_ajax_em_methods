51bc79e64d883c76d244d53e8af5a59ff6699744
Andrew Martin <andy123@stanford.edu>
2022-12-07 16:07:59 -0800

    adding survey hook example

--- TestAjaxEMMethods.php
+++ TestAjaxEMMethods.php
@@ -11,11 +11,12 @@ class TestAjaxEMMethods extends \ExternalModules\AbstractExternalModule {
 
 	}
 
-    public function redcap_data_entry_form_top($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $repeat_instance = 1) {
+    public function redcap_data_entry_form_top($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash, $response_id, $repeat_instance = 1) {
         echo $this->initializeJavascriptModuleObject();
         // Data to add to object
-        $data = [ "Foo" => "Bar"];
+        $data = [ "Survey" => "Test"];
         ?>
+        <script src="<?=$this->getUrl("assets/test1.js",true)?>"></script>
         <script>
             $(function() {
                 const module = <?=$this->getJavascriptModuleObjectName()?>;
@@ -23,9 +24,27 @@ class TestAjaxEMMethods extends \ExternalModules\AbstractExternalModule {
                 module.afterRender(<?=$this->getJavascriptModuleObjectName()?>.InitFunction);
             })
         </script>
-        <script src="<?=$this->getUrl("assets/test1.js",true)?>"></script>
         <?php
     }
+
+
+    public function redcap_survey_page_top($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $repeat_instance = 1) {
+        echo $this->initializeJavascriptModuleObject();
+        // Data to add to object
+        $data = [ "Foo" => "Bar"];
+        ?>
+        <script src="<?=$this->getUrl("assets/test1.js",true)?>"></script>
+        <script>
+            $(function() {
+                const module = <?=$this->getJavascriptModuleObjectName()?>;
+                module.data = <?=json_encode($data)?>;
+                module.afterRender(<?=$this->getJavascriptModuleObjectName()?>.InitFunction);
+                // module.ajax("Survey", module.data)
+            })
+        </script>
+        <?php
+    }
+
 
     public function redcap_module_ajax($action, $payload, $project_id, $record, $instrument, $event_id, $repeat_instance,
         $survey_hash, $response_id, $survey_queue_hash, $page, $page_full, $user_id, $group_id)
--- assets/test1.js
+++ assets/test1.js
@@ -3,7 +3,28 @@
 Object.assign(ExternalModules.Stanford.TestAjaxEMMethods, {
    "assinged": "object",
     InitFunction: function(){
-       console.log("Init")
+       console.log("Init");
+
+       let $this = ExternalModules.Stanford.TestAjaxEMMethods;
+       $this.ajax('MyAction', this.data).then(function(response) {
+            // Process response
+            console.log("Result: ", response);
+        }).catch(function(err) {
+            // Handle error
+            console.log(err);
+        });
+
+
+    },
+    SurveyAjax: function(){
+        parent.ajax('survey', this.data).then(function(response) {
+            // Process response
+            console.log("Result: ", response);
+        }).catch(function(err) {
+            // Handle error
+            console.log(err);
+        });
+
     }
 });
 
