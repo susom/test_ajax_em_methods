21f1956645b8b83c8185b1f240feb87ac8b434ea
Andrew Martin <andy123@stanford.edu>
2022-12-08 09:51:09 -0800

    cleaning up and adding object alias

--- assets/test1.js
+++ assets/test1.js
@@ -1,30 +1,34 @@
-// Playing around with best methods to extend JSMO with custom functions
+// Playing around with a standard methods to extend JSMO with custom functions
+;{
+    // Define the jsmo in IIFE so we can reference object in our new function methods
+    const jsmo = ExternalModules.Stanford.TestAjaxEMMethods;
 
-Object.assign(ExternalModules.Stanford.TestAjaxEMMethods, {
-   "assinged": "object",
-    InitFunction: function(){
-       console.log("Init");
+    // Extend the official JSMO with new methods
+    Object.assign(jsmo, {
+        "exampleProperty": "exampleValue",
 
-       let $this = ExternalModules.Stanford.TestAjaxEMMethods;
-       $this.ajax('MyAction', this.data).then(function(response) {
-            // Process response
-            console.log("Result: ", response);
-        }).catch(function(err) {
-            // Handle error
-            console.log(err);
-        });
+        InitFunction: function () {
+            console.log("Example Init Function");
 
+            // Note use of jsmo to call methods
+            jsmo.ajax('MyAction', this.data).then(function (response) {
+                // Process response
+                console.log("Ajax Result: ", response);
+            }).catch(function (err) {
+                // Handle error
+                console.log(err);
+            });
+        },
 
-    },
-    SurveyAjax: function(){
-        parent.ajax('survey', this.data).then(function(response) {
-            // Process response
-            console.log("Result: ", response);
-        }).catch(function(err) {
-            // Handle error
-            console.log(err);
-        });
-
-    }
-});
-
+        SurveyAjax: function () {
+            console.log("Example SurveyAjax Function");
+            jsmo.ajax('MyAction', this.data).then(function (response) {
+                // Process response
+                console.log("Survey Ajax Result: ", response);
+            }).catch(function (err) {
+                // Handle error
+                console.log(err);
+            });
+        }
+    });
+}
